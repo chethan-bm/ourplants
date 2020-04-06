b017cbdad95138ad73982c8487839dce52394dc0
chethan-bm <chethanbhavikatti@gmail.com>
2020-04-06 14:33:03 +0530

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,79 @@
+import 'package:ourplants/data/top_categories_model.dart';
+List<TopCategoriesModel> getTopCategoriesData(){
+
+  List<TopCategoriesModel> topCategories = new List();
+  TopCategoriesModel topCategoriesModel = new TopCategoriesModel();
+
+  //1
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  //2
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+
+  topCategoriesModel.imgAssetPath = "images/plant01.png";
+  topCategoriesModel.label = "Regular Gift";
+
+  topCategories.add(topCategoriesModel);
+  topCategoriesModel = new TopCategoriesModel();
+
+
+  return topCategories;
+
+}
---
+++ lib/data/top_categories_model.dart
@@ -0,0 +1,6 @@
+class TopCategoriesModel{
+
+  String label;
+  String imgAssetPath;
+
+}
